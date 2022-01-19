422671b7533d0c219eff25aad54b043486751747
Nazir Khashiev <nxashiev@gmail.com>
2022-01-19 14:53:33 +0300

    fixed 1 module 4 task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,4 @@
 function checkSpam(str) {
   let str2 = str.toLowerCase();
-  if (str2.includes("1xbet") || str2.includes("xxx")) {
-    return true;
-  }
-  return false;
+  return str2.includes("1xbet") || str2.includes("xxx");
 }
