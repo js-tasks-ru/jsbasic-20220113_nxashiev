d5b8cb93a60f6040ae9743df26da371c3bed38db
Nazir Khashiev <nxashiev@gmail.com>
2022-01-29 19:52:26 +0300

    solution for 3 module 4 task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let newArr = users.filter((user) => user.age <= age);
+
+  let result = newArr.map((user, index) =>
+    index < newArr.length - 1
+      ? user.name + ", " + user.balance + "\n"
+      : user.name + ", " + user.balance
+  );
+
+  return result.join("");
 }
