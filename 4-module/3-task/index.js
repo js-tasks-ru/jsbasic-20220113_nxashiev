cf484121dacb5f8cf15934d875d08ac53a8374db
Nazir Khashiev <nxashiev@gmail.com>
2022-02-02 17:15:13 +0300

    solution for 4 module 3 task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  for (let elem of table.lastElementChild.children) {
+    //
+    if (elem.lastElementChild.hasAttribute("data-available")) {
+      elem.classList.add(
+        elem.lastElementChild.getAttribute("data-available") === "true"
+          ? "available"
+          : "unavailable"
+      );
+    } else {
+      elem.setAttribute("hidden", "");
+    }
+    //
+    if (elem.children[2].textContent === "m") {
+      elem.classList.add("male");
+    } else {
+      elem.classList.add("female");
+    }
+    //
+    if (parseInt(elem.children[1].textContent) < 18) {
+      elem.style.textDecoration = "line-through";
+    }
+  }
 }
