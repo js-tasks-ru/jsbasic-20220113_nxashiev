46c7f26d3beee516d6ed6feecdfa3a66b2cc7379
Nazir Khashiev <nxashiev@gmail.com>
2022-02-02 12:38:21 +0300

    solution for 4 module 1 task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("UL");
+
+  for (let item of friends) {
+    ul.innerHTML += `<li>${item.firstName} ${item.lastName}</li>`;
+  }
+
+  return ul;
 }
