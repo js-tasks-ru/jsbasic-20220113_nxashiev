d3040bc7f0322fcda8e72f306713ebbb41bc4e64
Nazir Khashiev <nxashiev@gmail.com>
2022-02-15 21:15:21 +0300

    minor improvements

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,37 +1,47 @@
 import createElement from "../../assets/lib/create-element.js";
 
 export default class ProductCard {
-  constructor(product) {
-    this.elem = createElement(`
-    <div class="card">
-    <div class="card__top">
-        <img src="/assets/images/products/${product.image}"
-        class="card__image" alt="product">
-        <span class="card__price">€${product.price.toFixed(2)}</span>
-    </div>
-    <div class="card__body">
-        <div class="card__title">${product.name}</div>
-        <button type="button" class="card__button">
-            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-        </button>
-    </div>
-    </div>
-    `);
+  elem = null;
+  #product = null;
 
-    this.id = product.id;
-    this.getButton().addEventListener("click", this.onBtnClick);
+  constructor(product) {
+    this.#product = product;
+
+    this.elem = this.#template();
+
+    this.#getButton().addEventListener("click", this.#onBtnClick);
   }
 
-  onBtnClick = (event) => {
+  #template() {
+    return createElement(`
+      <div class="card">
+      <div class="card__top">
+          <img src="/assets/images/products/${this.#product.image}"
+          class="card__image" alt="product">
+          <span class="card__price">€${this.#product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${this.#product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+      </div>
+      `);
+  }
+
+  #onBtnClick = (event) => {
     const btnClickEvent = new CustomEvent("product-add", {
       bubbles: true,
-      detail: this.id,
+      detail: this.#product.id,
     });
 
     this.elem.dispatchEvent(btnClickEvent);
+
+    console.log(event.detail);
   };
 
-  getButton() {
+  #getButton() {
     return this.elem.querySelector(".card__button");
   }
 }
