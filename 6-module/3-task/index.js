8e69386d7b17769e397fa03c47569186e4fad7fa
Nazir Khashiev <nxashiev@gmail.com>
2022-02-16 20:14:22 +0300

    6 module 3 task half-solution

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,42 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+
+    this.elem = createElement(`
+      <div class="carousel">
+      <div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+      <div class="carousel__arrow carousel__arrow_left">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+
+      <div class="carousel__inner">
+        ${this.template()}
+      </div>
+    </div>
+    `);
+  }
+
+  template() {
+    return this.slides
+      .map((slide) => {
+        return `
+      <div class="carousel__slide" data-id="penang-shrimp">
+      <img src="/assets/images/carousel/${slide.image}"
+       class="carousel__img" alt="slide">
+      <div class="carousel__caption">
+        <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+        <div class="carousel__title">${slide.name}</div>
+        <button type="button" class="carousel__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+      </div>
+      `;
+      })
+      .join("");
   }
 }
